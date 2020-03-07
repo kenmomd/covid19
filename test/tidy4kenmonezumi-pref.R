b1c23a6cd449ae834afdf9ef945613be79d0658a
kenmomd <61652491+kenmomd@users.noreply.github.com>
2020-03-07 13:29:48 +0900

    県別検査数csvのRによる縦横変換スクリプト(tidy data)の微修正

--- test/tidy4kenmonezumi-pref.R
+++ test/tidy4kenmonezumi-pref.R
@@ -5,7 +5,7 @@ library(tidyverse)
 setwd("~/2019-nCov/covid19/test")
 getwd()
 df <- read.csv("pcr_pref_data_daily_lerp_kenmonezumi.csv")
-head(prefts, n = 3)
+head(df, n = 3)
 
 # 縦横変換、X削除
 prefdf <- gather(df, key = date, value = testing, -pref)
