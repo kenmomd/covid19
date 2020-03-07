42043e87eec8dbc0b5744b9b93eef5f491476b43
kenmomd <61652491+kenmomd@users.noreply.github.com>
2020-03-07 15:06:46 +0900

    ggplotによるグラフ化

--- test/test.R
+++ test/test.R
@@ -1,19 +1,23 @@
 # ライブラリの読み込み
 library(tidyverse)
-library(ggsci)
-# データの読み込み
 
+# データの読み込み
 setwd("~/2019-nCov/covid19/test")
 prefdf <- read.csv("tidy_pcr_pref_daily_lerp.csv")
+# 一旦文字列型に変換
+prefdf$date <- as.character(prefdf$date) 
+# 日付の型に変換
+prefdf$date <- as.Date(prefdf$date, "%Y-%m-%d")
 head(prefdf, n = 3)
-
+sapply(prefdf, class)
 
 g <- ggplot(prefdf, aes(x = date, y = testing, color = pref))
 g <- g + geom_line()
 #g <- g + scale_color_jama()
 plot(g)
 
-
+# 県別 プロット
 p <- ggplot(data = prefdf, mapping = aes(x = date, y = testing)) +
-  geom_line() + facet_wrap(~ pref, ncol=6)
+  geom_line() + facet_wrap(~ pref, ncol=5) + theme_minimal()
 plot(p)
+
