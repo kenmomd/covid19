8592781a82376354fc52408ca2aa9389a8ef4724
kenmomd <61652491+kenmomd@users.noreply.github.com>
2020-03-07 15:06:10 +0900

    csv変換スクリプトの修正並び変えとデータ型

--- test/tidy4kenmonezumi-pref.R
+++ test/tidy4kenmonezumi-pref.R
@@ -7,8 +7,24 @@ getwd()
 df <- read.csv("pcr_pref_data_daily_lerp_kenmonezumi.csv")
 head(df, n = 3)
 
-# 縦横変換、X削除
+# 縦横変換、X削除、列並び変え
 prefdf <- gather(df, key = date, value = testing, -pref)
 prefdf <- dplyr::mutate_all(prefdf, ~gsub(.,pattern="X",replacement = ""))
+prefdf <- prefdf[,c(2,1,3)]
 
+# 数字ではas.Dateできないので文字列型に変換
+prefdf$date <- as.character(prefdf$date) 
+# 日付の型に変換
+prefdf$date <- as.Date(prefdf$date, "%Y%m%d")
+# 数値型（整数）、因子型に変換
+prefdf$testing <- as.integer(prefdf$testing)
+prefdf$pref <- as.factor(prefdf$pref)
+
+# データフレームの確認
+head(prefdf, n = 3)
+sapply(prefdf, class)
+
+# csvに保存
 write.csv(prefdf, "./tidy_pcr_pref_daily_lerp.csv",row.names=FALSE)
+
+
